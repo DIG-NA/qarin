4d6adf76e7e348e3a1f79d7e026870aa45e28ed0
ELMUTAZ-ABDELRAHMAN <xoyenterprise@gmail.com>
2025-09-25 07:34:02 +0400

    - styled the field-buttons

--- lib/fieldview.dart
+++ lib/fieldview.dart
@@ -40,132 +40,268 @@ class _FieldViewState extends State<FlowView> {
   }
 }
 
+// original implementaion: the dragable hovering card
+// class Field extends StatelessWidget {
+//   const Field({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MouseRegion(
+//       cursor: SystemMouseCursors.grab,
+//       child: SizedBox(
+//         height: _FieldViewState().widgetHeight,
+
+//         // width: _FieldViewState().widgetWidth,
+//         child: Center(
+//           child: Consumer<FieldViewModel>(
+//             builder: (context, v, child) {
+//               Map<String, dynamic> t = {
+//                 "id": "${v.id}",
+//                 "timestamp": v.time,
+//                 "location": {"lat": v.lat, "lng": v.lng},
+//                 "type": "DroneAttack",
+//                 "side": v.side,
+//                 "url": v.url,
+//                 "description": v.description,
+//               };
+//               return Material(
+//                 shape: Theme.of(context).cardTheme.shape,
+//                 child: Padding(
+//                   padding: const EdgeInsets.all(10.0),
+//                   child: Column(
+//                     mainAxisAlignment: MainAxisAlignment.start,
+//                     crossAxisAlignment: CrossAxisAlignment.start,
+//                     children: [
+//                       TextFormField(
+//                         onChanged: (value) {
+//                           v.id = int.tryParse(value) ?? 0;
+//                           v.notify();
+//                         },
+//                         decoration: InputDecoration(labelText: 'id'),
+//                         onEditingComplete:
+//                             () => FocusScope.of(context).nextFocus(),
+//                       ),
+//                       // will probly use this or a different implemntaion for the time thing
+//                       // TextFormField(
+//                       //   onChanged: (value) {
+//                       //     v.time = value;
+//                       //     v.notify();
+//                       //   },
+//                       //   decoration: InputDecoration(labelText: 'time'),
+//                       //   onEditingComplete:
+//                       //       () => FocusScope.of(context).nextFocus(),
+//                       // ),
+
+//                       //chatgpt
+//                       TextFormField(
+//                         readOnly: true,
+//                         decoration: const InputDecoration(labelText: 'Time'),
+//                         controller: TextEditingController(text: '${v.time}'),
+//                         onTap: () async {
+//                           final picked = await showDatePicker(
+//                             context: context,
+//                             firstDate: DateTime(2020),
+//                             lastDate: DateTime(2030),
+//                           );
+//                           if (picked != null) {
+//                             v.time = picked;
+//                             v.notify();
+//                           }
+//                         },
+//                       ),
+//                       TextFormField(
+//                         onChanged: (value) {
+//                           v.url = value;
+//                           v.notify();
+//                         },
+//                         decoration: InputDecoration(labelText: 'url'),
+//                         onEditingComplete:
+//                             () => FocusScope.of(context).nextFocus(),
+//                       ),
+//                       // TextFormField(
+//                       //   onChanged: (value) {
+//                       //     v.type = ;
+//                       //   },
+//                       //   decoration: InputDecoration(labelText: 'type'),
+//                       //   onEditingComplete:
+//                       //       () => FocusScope.of(context).nextFocus(),
+//                       // ),
+//                       TextFormField(
+//                         onChanged: (value) {
+//                           v.description = value;
+//                           v.notify();
+//                         },
+//                         decoration: InputDecoration(labelText: 'description'),
+//                       ),
+//                       Row(
+//                         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//                         crossAxisAlignment: CrossAxisAlignment.center,
+//                         children: [
+//                           Padding(
+//                             padding: const EdgeInsets.symmetric(vertical: 6),
+//                             child: Switch(
+//                               value: v.side == 'saf',
+//                               onChanged: (value) {
+//                                 v.side == Side.saf.name
+//                                     ? v.side = Side.rsf.name
+//                                     : v.side = Side.saf.name;
+//                                 v.notify();
+//                               },
+//                             ),
+//                           ),
+//                           Builder(
+//                             builder: (context) {
+//                               return IconButton(
+//                                 onPressed: () => writeData(t),
+//                                 icon: Icon(Icons.upload),
+//                               );
+//                             },
+//                           ),
+//                         ],
+//                       ),
+//                     ],
+//                   ),
+//                 ),
+//               );
+//             },
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
 class Field extends StatelessWidget {
   const Field({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MouseRegion(
-      cursor: SystemMouseCursors.grab,
-      child: SizedBox(
-        height: _FieldViewState().widgetHeight,
-        width: _FieldViewState().widgetWidth,
+    return Consumer<FieldViewModel>(
+      builder: (context, v, child) {
+        Map<String, dynamic> t = {
+          "id": "${v.id}",
+          "timestamp": v.time,
+          "location": {"lat": v.lat, "lng": v.lng},
+          "type": "DroneAttack",
+          "side": v.side,
+          "url": v.url,
+          "description": v.description,
+        };
+        return Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TextFormField(
+              onChanged: (value) {
+                v.id = int.tryParse(value) ?? 0;
+                v.notify();
+              },
+              decoration: InputDecoration(labelText: 'id'),
+              onEditingComplete: () => FocusScope.of(context).nextFocus(),
+            ),
+            // will probly use this or a different implemntaion for the time thing
+            // TextFormField(
+            //   onChanged: (value) {
+            //     v.time = value;
+            //     v.notify();
+            //   },
+            //   decoration: InputDecoration(labelText: 'time'),
+            //   onEditingComplete:
+            //       () => FocusScope.of(context).nextFocus(),
+            // ),
 
-        child: Center(
-          child: Consumer<FieldViewModel>(
-            builder: (context, v, child) {
-              Map<String, dynamic> t = {
-                "id": "${v.id}",
-                "timestamp": v.time,
-                "location": {"lat": v.lat, "lng": v.lng},
-                "type": "DroneAttack",
-                "side": v.side,
-                "url": v.url,
-                "description": v.description,
-              };
-              return Material(
-                shape: Theme.of(context).cardTheme.shape,
-                child: Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      TextFormField(
-                        onChanged: (value) {
-                          v.id = int.tryParse(value) ?? 0;
-                          v.notify();
-                        },
-                        decoration: InputDecoration(labelText: 'id'),
-                        onEditingComplete:
-                            () => FocusScope.of(context).nextFocus(),
+            //chatgpt
+            TextFormField(
+              readOnly: true,
+              decoration: const InputDecoration(labelText: 'Time'),
+              controller: TextEditingController(text: '${v.time}'),
+              onTap: () async {
+                final picked = await showDatePicker(
+                  context: context,
+                  firstDate: DateTime(2020),
+                  lastDate: DateTime(2030),
+                );
+                if (picked != null) {
+                  v.time = picked;
+                  v.notify();
+                }
+              },
+            ),
+            TextFormField(
+              onChanged: (value) {
+                v.url = value;
+                v.notify();
+              },
+              decoration: InputDecoration(labelText: 'url'),
+              onEditingComplete: () => FocusScope.of(context).nextFocus(),
+            ),
+            // TextFormField(
+            //   onChanged: (value) {
+            //     v.type = ;
+            //   },
+            //   decoration: InputDecoration(labelText: 'type'),
+            //   onEditingComplete:
+            //       () => FocusScope.of(context).nextFocus(),
+            // ),
+            TextFormField(
+              onChanged: (value) {
+                v.description = value;
+                v.notify();
+              },
+              decoration: InputDecoration(labelText: 'description'),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: Card(
+                      color: Colors.black,
+                      shape: BeveledRectangleBorder(
+                        borderRadius: BorderRadius.circular(1),
                       ),
-                      // will probly use this or a different implemntaion for the time thing
-                      // TextFormField(
-                      //   onChanged: (value) {
-                      //     v.time = value;
-                      //     v.notify();
-                      //   },
-                      //   decoration: InputDecoration(labelText: 'time'),
-                      //   onEditingComplete:
-                      //       () => FocusScope.of(context).nextFocus(),
-                      // ),
-
-                      //chatgpt
-                      TextFormField(
-                        readOnly: true,
-                        decoration: const InputDecoration(labelText: 'Time'),
-                        controller: TextEditingController(text: '${v.time}'),
-                        onTap: () async {
-                          final picked = await showDatePicker(
-                            context: context,
-                            firstDate: DateTime(2020),
-                            lastDate: DateTime(2030),
-                          );
-                          if (picked != null) {
-                            v.time = picked;
+                      child: Transform.scale(
+                        scale: .7,
+                        child: Switch(
+                          value: v.side == 'saf',
+                          onChanged: (value) {
+                            v.side == Side.saf.name
+                                ? v.side = Side.rsf.name
+                                : v.side = Side.saf.name;
                             v.notify();
-                          }
-                        },
+                          },
+                        ),
                       ),
-                      TextFormField(
-                        onChanged: (value) {
-                          v.url = value;
-                          v.notify();
-                        },
-                        decoration: InputDecoration(labelText: 'url'),
-                        onEditingComplete:
-                            () => FocusScope.of(context).nextFocus(),
-                      ),
-                      // TextFormField(
-                      //   onChanged: (value) {
-                      //     v.type = ;
-                      //   },
-                      //   decoration: InputDecoration(labelText: 'type'),
-                      //   onEditingComplete:
-                      //       () => FocusScope.of(context).nextFocus(),
-                      // ),
-                      TextFormField(
-                        onChanged: (value) {
-                          v.description = value;
-                          v.notify();
-                        },
-                        decoration: InputDecoration(labelText: 'description'),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 6),
-                            child: Switch(
-                              value: v.side == 'saf',
-                              onChanged: (value) {
-                                v.side == Side.saf.name
-                                    ? v.side = Side.rsf.name
-                                    : v.side = Side.saf.name;
-                                v.notify();
-                              },
-                            ),
-                          ),
-                          Builder(
-                            builder: (context) {
-                              return IconButton(
-                                onPressed: () => writeData(t),
-                                icon: Icon(Icons.upload),
-                              );
-                            },
-                          ),
-                        ],
-                      ),
-                    ],
+                    ),
                   ),
-                ),
-              );
-            },
-          ),
-        ),
-      ),
+
+                  Expanded(
+                    flex: 6,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 1),
+                      child: Card(
+                        color: Colors.black,
+                        shape: BeveledRectangleBorder(
+                          borderRadius: BorderRadius.circular(1),
+                        ),
+                        child: IconButton(
+                          onPressed: () => writeData(t),
+                          icon: const Icon(
+                            Icons.upload,
+                            size: 27,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 }
